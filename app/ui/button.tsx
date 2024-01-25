82abb20c489943c8f77843e4e59c2a520fdbfaea
Abdul Rehman <92783456+TheAbdulRehman@users.noreply.github.com>
2024-01-25 23:03:31 +0500

    change the button now

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,19 +1,19 @@
-import clsx from 'clsx';
+import React, { ReactNode } from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+interface ButtonProps {
+  onClick?: () => void;
+  children: ReactNode;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
   return (
     <button
-      {...rest}
-      className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className,
-      )}
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
       {children}
     </button>
   );
-}
+};
+
+export default Button;
